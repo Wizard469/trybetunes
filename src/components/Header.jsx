ff00b346886527d024b9b32e803816a40d3f337a
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-07-04 17:07:08 -0300

    Requirement four

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import Loading from './Loading';
 import { getUser } from '../services/userAPI';
 
@@ -24,7 +25,16 @@ class Header extends Component {
     const { user, loading } = this.state;
     return (
       <header data-testid="header-component">
-        { loading ? <Loading /> : <h4 data-testid="header-user-name">{ user.name }</h4>}
+        { loading ? <Loading /> : (
+          <>
+            <h4 data-testid="header-user-name">{ user.name }</h4>
+            <nav>
+              <NavLink to="/search" data-testid="link-to-search">Search</NavLink>
+              <NavLink to="/favorites" data-testid="link-to-favorites">Favorites</NavLink>
+              <NavLink to="/profile" data-testid="link-to-profile">Profile</NavLink>
+            </nav>
+          </>
+        )}
       </header>
     );
   }
